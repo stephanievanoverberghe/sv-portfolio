5e361e40d78269b3dbc65ba9b5040d69ffe06193
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-05-05 14:36:42 +0200

    fix: add file projects

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     plugins: ['prettier-plugin-tailwindcss'],
+    trailingComma: ['es5'],
+    semi: true,
+    tabWidth: 4,
+    useTabs: true,
+    bracketSpacing: true,
+    arrowParens: ['always'],
+    printWidth: 200
 }
