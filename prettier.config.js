cfff7e89839b4fc5c2b9bbe260d2d72a5cab5248
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-05-05 16:24:12 +0200

    fix: add slides

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,3 @@
 module.exports = {
     plugins: ['prettier-plugin-tailwindcss'],
-    trailingComma: ['es5'],
-    semi: true,
-    tabWidth: 4,
-    useTabs: true,
-    bracketSpacing: true,
-    arrowParens: ['always'],
-    printWidth: 200
 }
